e9bb4fcc4ff6256e015ebb4727c8d20dde3e2a6e
Phil Lang <plangphoto@gmail.com>
2017-06-26 11:26:59 +0100

    Fizzbuzz working including tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+  elsif number % 5 ==0
+    puts "buzz"
+  elsif number % 3 == 0
+    puts "fizz"
+  else
+    puts number
+  end
 end
