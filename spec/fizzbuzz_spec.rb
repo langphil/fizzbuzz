6d8ec01f800bbbac9d8afe9f0aec03409c8f44de
Phil Lang <plangphoto@gmail.com>
2017-06-28 11:33:36 +0100

    refactored production code and tests to factor in Integer Class

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,23 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
   end
 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "number" when passed a number' do
+    expect(4.fizzbuzz).to eq 4
+  end
+
+  it 'returns nil when a passed a nil argument' do
+    expect(nil).to eq nil
   end
 end
