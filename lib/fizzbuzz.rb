80b4677bde8c2cc66239e71991443187649fa6b9
Phil Lang <plangphoto@gmail.com>
2017-06-28 11:39:02 +0100

    indent

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 # To test in IRB use require 'Fizzbuzz' and run this - (1..100).each { |i| puts fizzbuzz(i) }
 class Integer
-def fizzbuzz
-  str = ""
-  return nil if self.nil?
-  str += "fizz" if self % 3 == 0
-  str += "buzz" if self % 5 == 0
-  str.empty? ? self : str
-end
+  def fizzbuzz
+    str = ""
+    return nil if self.nil?
+    str += "fizz" if self % 3 == 0
+    str += "buzz" if self % 5 == 0
+    str.empty? ? self : str
+  end
 end
